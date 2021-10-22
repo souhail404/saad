444dd2a5b8745fa52c10cb76dde3df1fe7ab9dda
souhail404 <elabbouisouhail@gmail.com>
2021-10-22 14:41:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// const carousel=document.querySelector('.slide');
+// const slideimg=document.querySelectorAll('.slide img');
+
+// const nxtbtn =document.querySelector('#nextbtn');
+// const prvbtn =document.querySelector('#prevbtn');
+
+// let counter=0;
+
+// const size= slideimg[0].clientWidth;
+
+// carousel.style.transform ='translateX('+ ( - size * counter) + 'px)';
+
+// nxtbtn.addEventListener('click' ,()=>{
+//     carousel.style.transition ='transform 0.4s ease-in-out';
+//     if(counter <= 0){ counter0++; } else{counter--}
+    
+//     carousel.style.transform ='translateX('+ ( - size * counter) + 'px)';
+// })
+
+// prvbtn.addEventListener('click' ,()=>{
+//     carousel.style.transition ='transform 0.4s ease-in-out';
+//     counter--;
+//     carousel.style.transform ='translateX('+ ( - size * counter) + 'px)';
+// });
+ 
+
+
